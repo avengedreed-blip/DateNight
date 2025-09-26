6f6a7b9f85623eb3b298deef260492cd78de1e09
avengedreed-blip <avengedreed@gmail.com>
2025-09-26 13:16:56 -0400

    Add splash screen component

---
+++ src/screens/SplashScreen.jsx
@@ -0,0 +1,31 @@
+import React, { memo } from "react";
+
+const SplashScreen = memo(() => (
+  <div
+    className="screen enter"
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <h1
+      className="brand-title animate-in"
+      style={{
+        fontSize: "clamp(3rem, 10vw, 6rem)",
+        color: "var(--ring)",
+      }}
+    >
+      Date Night
+    </h1>
+    <p
+      className="animate-in delay-1"
+      style={{ color: "var(--text-weak)", fontSize: "1.25rem" }}
+    >
+      Loading the fun...
+    </p>
+  </div>
+));
+
+export default SplashScreen;
